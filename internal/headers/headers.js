316d24c6626fa173b7a248040bcb2a86a1e5a72e
Paul-HenryN <ngpaulhenry@gmail.com>
2025-10-18 14:29:14 +0200

    feat: implement header parsing logic and update tests for new behavior

--- internal/headers/headers.js
+++ internal/headers/headers.js
@@ -1,12 +1,56 @@
+import { MalformedHeadersError } from "./headers.errors";
+import { Request } from "../request/request.js";
+
 export class Headers {
   #headersObj = {};
 
   /**
-   * Parses Http headers from a string input
+   * @typedef ParseResponse
+   * @property {number} charsRead
+   * @property {boolean} done
+   *
+   * Parses Http headers from a string input.
+   * The input might be incomplete.
    * @param {string} input
-   * @returns {Headers}
+   * @returns {ParseResponse}
+   * @throws {MalformedHeadersError}
    */
-  static parse(input) {}
+  parse(input) {
+    let charsRead = 0;
+    let done = false;
+
+    while (true) {
+      const lineTerminatorIdx = input.indexOf(Request.SEPARATOR, charsRead);
+
+      if (lineTerminatorIdx === -1) {
+        break;
+      }
+
+      if (lineTerminatorIdx === charsRead) {
+        done = true;
+        break;
+      }
+
+      const line = input.slice(charsRead, lineTerminatorIdx);
+      const delimiterIdx = line.indexOf(":");
+
+      if (delimiterIdx === -1) {
+        throw new MalformedHeadersError();
+      }
+
+      if (line[delimiterIdx - 1] === " ") {
+        throw new MalformedHeadersError();
+      }
+
+      const headerName = line.slice(0, delimiterIdx).trimStart();
+      const headerValue = line.slice(delimiterIdx + 1).trim();
+
+      this.set(headerName, headerValue);
+      charsRead += lineTerminatorIdx + Request.SEPARATOR.length;
+    }
+
+    return { charsRead, done };
+  }
 
   /**
    * Returns the header value associated with
@@ -16,5 +60,15 @@ export class Headers {
    */
   get(key) {
     return this.#headersObj[key];
+  }
+
+  /**
+   * Sets the header value associated with
+   * a particular key
+   * @param {string} key
+   * @param {string} value
+   */
+  set(key, value) {
+    this.#headersObj[key] = value;
   }
 }
--- internal/headers/headers.test.js
+++ internal/headers/headers.test.js
@@ -4,21 +4,31 @@ import { expect } from "vitest";
 import { MalformedHeadersError } from "./headers.errors.js";
 
 test("Should parse single header correctly", () => {
-  const headers = Headers.parse("Host: localhost:42069\r\n\r\n");
+  const headers = new Headers();
 
-  expect(headers).toBeDefined();
+  const { charsRead, done } = headers.parse("Host: localhost:42069\r\n\r\n");
+
   expect(headers.get("Host")).toBe("localhost:42069");
+  expect(charsRead).toBe(23);
+  expect(done).toBe(true);
 });
 
 test("Should parse single header with valid extra spaces correctly", () => {
-  const headers = Headers.parse("     Host:      localhost:42069 \r\n\r\n");
+  const headers = new Headers();
 
-  expect(headers).toBeDefined();
+  const { charsRead, done } = headers.parse(
+    "     Host:      localhost:42069 \r\n\r\n"
+  );
+
   expect(headers.get("Host")).toBe("localhost:42069");
+  expect(charsRead).toBe(34);
+  expect(done).toBe(true);
 });
 
 test("Should throw MalformedHeadersError when there's an extra space after header name", () => {
+  const headers = new Headers();
+
   expect(() =>
-    Headers.parse("       Host : localhost:42069       \r\n\r\n")
+    headers.parse("     Host :      localhost:42069 \r\n\r\n")
   ).toThrow(MalformedHeadersError);
 });
